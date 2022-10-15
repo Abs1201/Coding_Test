c1f4ffda8b1a455a627a78e61b61207cfd27226b
KimSungHun <91719218+Abs1201@users.noreply.github.com>
2022-10-15 18:26:53 +0900

    Update 정수삼각형.cpp

--- Coding_Test/Level3/정수삼각형.cpp
+++ Coding_Test/Level3/정수삼각형.cpp
@@ -1,43 +1,46 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-int factorior(int n) {
-	int result = 1;
-	for (int i = 1; i <= n; i++) {
-		result *= i;
-	}
-	return result;
+int getBigger(int& a, int& b) {
+    return a > b ? a : b;
 }
 
-int sigma(int n) {
-	int result = 0;
-	for (int i = 0; i <= n; i++) {
-		result += i;
-	}
-	return result;
-}
 
-vector<int> check(5);
 int solution(vector<vector<int>> triangle) {
-	int answer = 0;
-	int level = 0;
-	for (int i = level; i <= level+1; i++) {
-		check.push_back(triangle[level][i]);
-		solution(triangle);
-	}
+    int answer = 0;
+    if (triangle.size() == 0) {
+        return 0;
+    }
+    else if (triangle.size() == 1) {
+        return triangle[0][0];
+    }
+
+    
+    vector<vector<int>> Sum = triangle;
 
 
+    int seq;
+    for (seq = 1; seq < triangle.size(); seq++) {
+        for(int i = 0; i <= seq; i++){
+            if (i == 0) {
+                Sum[seq][i] += Sum[seq-1][i];
+            }
+            else if (i == seq) {
+                Sum[seq][i] += Sum[seq - 1][i-1];
+            }
+            else {
+                Sum[seq][i] += getBigger(Sum[seq - 1][i-1], Sum[seq - 1][i]);
+            }
+            
+        }
+    }
 
-	return answer;
-}
+    for (int i = 0; i < triangle.size(); i++) {
+        answer = getBigger(answer, Sum[triangle.size() - 1][i]);
+    }
 
-int main(void) {
-
-	int a = 5;
-	cout << factorior(5) << endl;
-
-	return 0;
+    return answer;
 }
