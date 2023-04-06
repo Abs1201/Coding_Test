78e6811fddd982ccc58532263188dd6f397d1f7a
KimSungHun <kimtiger1201@naver.com>
2023-04-06 10:57:28 +0900

    error

---
+++ Coding_Test/Level2/english_follow_up.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+// template <class T>
+// ostream& operator<<(ostream& out, const vector<T>& values){
+//     copy(begin(value), end(value), ostream_iterator<T>(stream, ","));
+// }
+
+// vector<int> solution(int n, vector<string> words) {
+//     vector<int> answer;
+//     answer.push_back(1);
+//     answer.push_back(1);
+//     answer.push_back(1);
+
+//     return answer;
+// }
+
+int main(void){
+
+    vector<string> words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+    int n = 3;
+
+    //vector<int> answer = solution(n, words);
+
+    //cout << answer << endl;
+    cout <<"hi"; 
+    //<<endl;
+
+    return 0;
+}
--- Python_studying/stringFunction.py
+++ Python_studying/stringFunction.py
@@ -1,2 +1,9 @@
 # substring
 
+if __name__ == '__main__':
+    tmp = '0b10011010'
+    print(tmp[2:])
+    print(int(tmp, 2))
+    print(tmp[-5:])
+    print(tmp[-5:len(tmp)])
+    
