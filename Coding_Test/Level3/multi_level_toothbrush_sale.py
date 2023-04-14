80b42d490f9b3b21537e7127397ead6738c3b86a
KimSungHun <kimtiger1201@naver.com>
2023-04-14 15:24:09 +0900

    Create multi_level_toothbrush_sale.py

---
+++ Coding_Test/Level3/multi_level_toothbrush_sale.py
@@ -0,0 +1,9 @@
+def solution(enroll, referral, seller, amount):
+    answer = []
+    return answer
+
+
+if __name__ == '__main_':
+    print("hello")
+
+    #https://school.programmers.co.kr/learn/courses/30/lessons/77486
