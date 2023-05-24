d1f1220555a38701ef4b896af5bff743ff4498ee
KimSungHun <kimtiger1201@naver.com>
2023-05-24 20:02:11 +0900

    billiardsPrac

---
+++ Coding_Test/Level2/billiardsPrac.py
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+
+
+if __name__ == '__main__':
+    m = 10
+    n = 10
+    startX = 3
+    startY = 7
+    balls = [[7, 7], [2, 7], [7, 3]]
+    result = Solution(m,n,startX, startY, balls)
+    print(result)
