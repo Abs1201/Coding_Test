6000c18c93f929b91aca0ac44884789f9a799030
KimSungHun <kimtiger1201@naver.com>
2023-05-25 10:07:01 +0900

    Update billiardsPrac.py

--- Coding_Test/Level2/billiardsPrac.py
+++ Coding_Test/Level2/billiardsPrac.py
@@ -1,9 +1,24 @@
-class Solution {
-    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
-        int[] answer = {};
-        return answer;
-    }
-}
+def solution(m, n, startX, startY, balls):
+    answer = []
+
+    a = [-startX, startY]
+    b = [startX, -startY]
+    c = [m+startX, startY]
+    d = [startX, n+startY]
+
+    for ball in balls:
+        tmp = []
+        aLen = (ball[0]-a[0]) * (ball[0]-a[0]) + (ball[1]-a[1])*(ball[1]-a[1])
+        bLen = (ball[0]-a[0]) * (ball[0]-a[0]) + (ball[1]-a[1])*(ball[1]-a[1])
+        cLen = (ball[0]-a[0]) * (ball[0]-a[0]) + (ball[1]-a[1])*(ball[1]-a[1])
+        dLen = (ball[0]-a[0]) * (ball[0]-a[0]) + (ball[1]-a[1])*(ball[1]-a[1])
+        tmp.append(aLen)
+        tmp.append(bLen)
+        tmp.append(cLen)
+        tmp.append(dLen)
+        answer.append(min(tmp))
+
+    return answer
 
 
 
@@ -13,5 +28,5 @@ if __name__ == '__main__':
     startX = 3
     startY = 7
     balls = [[7, 7], [2, 7], [7, 3]]
-    result = Solution(m,n,startX, startY, balls)
+    result = solution(m,n,startX, startY, balls)
     print(result)
