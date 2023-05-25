c9f49ced007bdb174a640c6a6a3eddc8094a01ac
KimSungHun <kimtiger1201@naver.com>
2023-05-25 11:34:01 +0900

    Update billiardsPrac.py

--- Coding_Test/Level2/billiardsPrac.py
+++ Coding_Test/Level2/billiardsPrac.py
@@ -41,20 +41,28 @@ def solution(m, n, startX, startY, balls):
     d = [startX, (n-startY)+n]
 
     for ball in balls:
-        if isMatch(m,n,startX,startY,ball):
-            len = (startX-ball[0]) * (startX-ball[0]) + (startY-ball[1])*(startY-ball[1])
-            answer.append(len)
-            continue
         tmp = []
         aLen = (ball[0]-a[0]) * (ball[0]-a[0]) + (ball[1]-a[1])*(ball[1]-a[1])
         bLen = (ball[0]-b[0]) * (ball[0]-b[0]) + (ball[1]-b[1])*(ball[1]-b[1])
         cLen = (ball[0]-c[0]) * (ball[0]-c[0]) + (ball[1]-c[1])*(ball[1]-c[1])
         dLen = (ball[0]-d[0]) * (ball[0]-d[0]) + (ball[1]-d[1])*(ball[1]-d[1])
+
         tmp.append(aLen)
         tmp.append(bLen)
         tmp.append(cLen)
         tmp.append(dLen)
-        print(tmp)
+
+        if isMatch(startX,startY,ball):
+            if startX < ball[0]:
+                cLen = max(tmp)
+            elif startX > ball[0]:
+                alen = max(tmp)
+            elif startY < ball[1]:
+                dLen = max(tmp)
+            elif startY > ball[1]:
+                bLen = max(tmp)
+
+
         answer.append(min(tmp))
 
     return answer
