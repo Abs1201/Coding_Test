044d4cab3ca67386d57434cfc95e95999a7b7ffa
KimSungHun <91719218+Abs1201@users.noreply.github.com>
2023-09-07 12:48:42 +0900

    .

---
+++ CSES/DP/DiceCombinations.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int mxN=1e6, m=1e9+7;
+vector<ll> dp(mxN, 0);
+
+int Dice(int n){ //it takes O(n^2)
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if(dp[n]!=0) return dp[n];
+    int tmp=0;
+    for(int i=1; i<=6; i++){
+        tmp=(tmp+Dice(n-i))%m;
+    }
+    dp[n]=tmp;
+    return tmp;
+}
+
+int main(void){
+    int n;
+    cin >> n;
+    //f(n)=f(n-1)+f(n-2)+f(n-3)...+f(n-6);
+    dp[0]=1;
+    cout << Dice(n);
+
+
+    return 0;
+}
---
+++ CSES/DP/MinimizingCoins.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int mxN=100, mxX=1e6, m=1e9+7;
+int n, c[mxN], x;
+ll dp[mxX+1];
+
+int main(void){
+    int n, x;
+    cin >> n >> x;
+    for(int i=0; i<n; i++){
+        cin >> c[i];
+    }
+    for(int i=1; i<=x; i++){
+        dp[i]=1e9;
+        for(int j=0; j<n; j++){
+            if(c[j]<=i)
+                dp[i]=min(dp[i], dp[i-c[j]]+1);
+        }
+    }
+
+    return 0;
+}
