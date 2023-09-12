2711f3b5d0f3e252ceec7b70ccd53bd6052c1dd0
KimSungHun <kimtiger1201@naver.com>
2023-09-12 19:56:47 +0900

    .

--- CSES2/SortingAndSearching/Apartments.cpp
+++ CSES2/SortingAndSearching/Apartments.cpp
@@ -3,9 +3,31 @@ using namespace std;
 
 #define ll long long
 const int mxN=2e5;
+int a[mxN], b[mxN];
 
 int main(void){
-    
+    int n, m, k;
+    cin >> n >> m >> k;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i=0; i<m; i++){
+        cin >> b[i];
+    }
+    sort(a, a+n);
+    sort(b, b+m);
+    int ans=0;
+    for(int i=0, j=0; i<n; i++){
+        while(j<m&&(a[i]-k>b[j])){
+            j++;
+        }
+        if(j>=m) break;
+        else if(a[i]+k>=b[j]){
+            ans++;
+            j++;
+        }
+    }
+    cout << ans;
 
     return 0;
 }
