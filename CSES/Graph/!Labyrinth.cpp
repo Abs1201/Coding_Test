b5951e9ee499c08488d3cdbd19d026eccd1f7558
KimSungHun <91719218+Abs1201@users.noreply.github.com>
2023-09-19 12:42:52 +0900

    .

--- CSES/Graph/!Labyrinth.cpp
+++ CSES/Graph/!Labyrinth.cpp
@@ -1,49 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-const int mxN=1e3, di[4]={1, 0, -1, 0}, dj[4]={0, 1, 0, -1};
-const char dc[4]={'U', 'R', 'D', 'L'};
-int n, m, si, sj, ti, tj;
-string s[mxN], p[mxN]; 
-
+const int mxN=1e3;
+int n, m;
+int si, sj, ti, tj, ni[4]={0,0,-1,1};, nj[4]={-1,1,0,0};
+char d[4]={'U', 'D', 'L', 'R'}; 
+string s[mxN];
+bool vis[i][j];
 bool e(int i, int j){
-    return i<n&&i>=0&&j<m&&j>=0&&s[i][j]!='.';
+    return i>=0&&i<n&&j>=0&&j<m&&s[i][j]=='.';
 }
-
 int main(void){
     cin >> n >> m;
     for(int i=0; i<n; i++){
         cin >> s[i];
-        for(int j=0; j<m; j++){
-            if(s[i][j]=='A')
-                si=i, sj=j;//, s[i][j]='.';
-            if(s[i][j]=='B')
-                ti=i, tj=j, s[i][j]='.';
+        if(s[i][j]='A'){
+            si=i, sj=j;
         }
-        p[i]=string(n,0);
     }
-    queue<ar<int,2>> qu;
+    queue<array<int ,2>> qu;
     qu.push({si, sj});
-    while(qu.size()){
-        ar<int, 2> u = qu.front();
-        u.pop();
-        for(int k=0; k<4; k++){
-            int ni= u[0]+di[k], nj=u[1]+dj[k];
-            if(!e(ni, nj))
-                continue;
-            qu.push({ni, nj});
-            s[ni][nj]='#';
-            p[ni][nj]=dc[k];
+    for(int i=0; i<4; i++){
+
+    }
+    
+    
+    vis[si][sj]=1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            bfs(i, j);
         }
     }
-    if(p[ti][tj]){
-        cout << "YES" << endl;
-        string t;
-        reverse(t.begin(), t.end());
-        cout << t.size() << endl;
-        cout << t;
-    }
+
 
     return 0;
 }
---
+++ CSES/Graph/Labyrinth_will.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mxN=1e3;
+int n, m, si, sj, ti, tj, di[4]={0,0,-1,1}, dj[4]={-1,1,0,0}, d[mxN][mxN];
+char dc[4]={'L', 'R', 'U', 'D'};
+string s[mxN], p[mxN];
+
+bool e(int i, int j){
+    return i>=0&&i<n&&j>=0&&j<m&&s[i][j]=='.';
+}
+
+int main(void){
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> s[i];
+        for(int j=0; j<m; j++){
+            if(s[i][j]=='A')
+                si=i, sj=j;
+            if(s[i][j]=='B')
+                ti=i, tj=j, s[i][j]='.';
+        }
+        p[i]=string(n,0);
+    }
+    queue<array<int, 2>> qu;
+    qu.push({si, sj});
+    while(qu.size()){
+        array<int ,2> ar=qu.front();
+        qu.pop();
+        for(int i=0; i<4; i++){
+            int ni=ar[0]+di[i], nj=ar[1]+dj[i];
+            if(!e(ni,nj))
+                continue;
+            qu.push({ni, nj});
+            s[ni][nj]='#';
+            d[ni][nj]=i;
+            p[ni][nj]=dc[i];
+        }
+    }
+    if(p[ti][tj]){
+        cout << "YES" << endl;
+        string t;
+        while(ti^si||tj^sj){
+            t+=p[ti][tj];
+            int dd=d[ti][tj]^2;
+            ti+=di[dd];
+            tj+=dj[dd];
+        }
+        reverse(t.begin(), t.end());
+        cout << t.size() << endl;
+        cout << t;
+    }
+    else{
+        cout << "NO";
+    }
+    
+    return 0;
+}
