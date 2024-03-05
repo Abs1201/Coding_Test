b1caab2f1e6de3fdfcdb1f016e3d247403c285b5
KimSungHun <kimtiger1201@naver.com>
2024-03-05 21:04:16 +0900

    .

--- CSES/Graph/HighScore.cpp
+++ CSES/Graph/HighScore.cpp
@@ -1,11 +1,10 @@
-//bellman-ford
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
-const int mxN=2500, mxM=5000;
 
-int n, m;
+const int mxN=2500, mxM=5000;
+ll n, m, ans[mxN];
 vector<array<ll, 2>> adj[mxN];
 ll d[mxN];
 
@@ -15,16 +14,37 @@ int main(void){
         int a, b, c;
         cin >> a >> b >> c, --a, --b;
         adj[a].push_back({c,b});
-    }
-
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<m; j++){
-            if(d[j] < d[j]+)
+    }    
+    memset(d, 0xc0, sizeof(d)); // representation of negative infinity
+    d[0]=0;
+    for(int i=0; i<2*n; i++){
+        bool ch=0;
+        for(int j=0; j<n; j++){
+            for(array<ll,2> a: adj[j]){
+                // d[a[1]]=max(d[a[1]], d[j]+a[0]);
+                if(d[j]+a[0]>d[a[1]]){
+                    if(a[1]==n-1){
+                        ch=1;
+                    }
+                    d[a[1]]=d[j]+a[0];
+                    
+                }
+            }
+            if(i==n-1&&ch){
+                cout << -1;
+                return 0;
+            }
         }
+        // ans[i]=d[n-1];
     }
+    cout << d[n-1];
+    // if(n>1&&ans[n-2]<ans[n-1]){
+    //     cout << -1;
+    // }
+    // else{
+    //     cout << ans[n-1];
+    // }
     
-
-
-
+    
     return 0;
 }
---
+++ CSES/Introductory/GrayCode.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int n;
+string ans[2e9];
+int main(void){
+    cin >> n;
+    for(int i=1; i<=pow(2,n), i++){
+        for(int j=n+1; j>0; j--){
+            
+        }
+    }
+    
+    
+    return 0;
+}
