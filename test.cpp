8a681b590474a3c20776d4142f7ad00f06c6904a
KimSungHun <kimtiger1201@naver.com>
2024-03-11 09:41:59 +0900

    .

--- test.cpp
+++ test.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <memory>
-#include <cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    long long tmp[1000];
-    cout << sizeof(tmp[0]) << endl;
-    cout << sizeof(tmp) << endl;
+int main(void){
 
+    set<int> s;
+    s.insert(10);
+    s.insert(20);
+    s.insert(20);
+    s.insert(20);
+    s.insert(40);
+    s.insert(70);
+    s.insert(40);
+    s.insert(50);
+
+    set<int>::iterator iter;
+    // int::iterator iter2;
+    for(iter=s.begin(); iter!=s.end(); iter++){
+        cout << *iter << endl;
+
+    }
+    if(iter==s.end()){
+        cout << "all g" << endl;
+    }
+    cout << *(iter-8) << endl;
     return 0;
 }
--- test/a.cpp
+++ test/a.cpp
@@ -1,22 +1,20 @@
-//grid path
+//josephus problem 1
 #include <bits/stdc++.h>
 using namespace std;
 
-const int M=1e9+7, mxN=1e3;
-int n, dp[mxN][mxN];
-string s[mxN];
+int n;
 
 int main(void){
     cin >> n;
+    set<int> s;
     for(int i=0; i<n; i++){
-        cin >> s[i];
+        s.insert(i);
     }
-    dp[0][0]=1;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            
-        }
-    }
+    set<int>::iterator iter;
+    // while(s.begin()!=s.end()){
+
+    // }
+    cout << sizeof(iter) << endl;
 
     return 0;
 }
