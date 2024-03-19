520bc79fa887c828d8ff6c676649f59aa6f2ed35
KimSungHun <kimtiger1201@naver.com>
2024-03-19 18:04:04 +0900

    .

--- test.cpp
+++ test.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void tmp(){
+    int a;
+    return;
+}
+
 int main(void){
 
     set<int> s;
@@ -17,11 +22,29 @@ int main(void){
     // int::iterator iter2;
     for(iter=s.begin(); iter!=s.end(); iter++){
         cout << *iter << endl;
-
+    }
+    for(iter=s.begin(); iter!=s.end(); ++iter){
+        cout << *iter << endl;
     }
     if(iter==s.end()){
         cout << "all g" << endl;
     }
-    cout << *(iter-8) << endl;
+
+    // prefix, postfix
+    int a=10;
+    int b=++a;
+    cout << a << " " << b << endl;
+
+    a=10;
+    b=a++;
+    cout << a << " " << b << endl;
+
+    // Q1
+    a=1, b=2;
+    a=a|b++;
+    cout << a << endl;
+
+    
+    
     return 0;
 }
