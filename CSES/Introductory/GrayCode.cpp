6872ff4ccf3853d9a89977e762e70084dc32cf59
KimSungHun <kimtiger1201@naver.com>
2024-03-31 18:29:31 +0900

    all g

--- CSES/Introductory/GrayCode.cpp
+++ CSES/Introductory/GrayCode.cpp
@@ -1,28 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
+#define ll long long
+const int mxN=1e7;
+
 int n;
-vector<string> ans={"0", "1"};
- 
- 
+string ans[mxN];
 int main(void){
     cin >> n;
-    
-    for(int i=0; i<n-1; i++){
-        vector<string> tmp(ans);
-        reverse(tmp.begin(), tmp.end());
-        for(string& s:ans){
-            s='0'+s;
+    bool b=1;
+    int s,s2;
+    for(int i=0; i<n; i++){
+        s=pow(2,i);
+        s2=s;
+        for(int j=0; j<pow(2,n); j++){
+            --s2;
+            if(b){
+                if(s2<=0){
+                    s2=s, b=!b;
+                }
+                ans[j]='0'+ans[j];
+            }
+            else{
+                if(s2<=0){
+                    s2=s, b=!b;
+                }
+                ans[j]='1'+ans[j];
+            }
         }
-        for(string& s:tmp){
-            s='1'+s;
-        }
-        ans.insert(ans.end(), tmp.begin(), tmp.end());
+    }
+
+    for(int i=0; i<pow(2,n); i++){
+        cout << ans[i] << endl;
     }
     
-    for(string s: ans){
-        cout << s << endl;
-    }
     
     return 0;
 }
