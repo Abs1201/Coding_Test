53df021a9155610f371f816b75e39cafa06db2e1
KimSungHun <kimtiger1201@naver.com>
2024-04-16 16:06:20 +0900

    Update DigitQueries.cpp

--- CSES/Introductory/DigitQueries.cpp
+++ CSES/Introductory/DigitQueries.cpp
@@ -15,18 +15,19 @@ int main(void){
     }
     int ans[mxN];
  
+    // for(int i=0; i<n; i++){
+    //     int sum=0;
+    //     int j=0, k, k2;
+    //     for(j=0, k=q[i]-sum; k>0; j++){
+    //         if(k>0) k2=k;
+    //         sum+=9*pow(10,j);
+    //     }
+    //     int q=
+
+    // }
     for(int i=0; i<n; i++){
-        int sum=0;
-        int j=0, k, k2;
-        for(j=0, k=q[i]-sum; k>0; j++){
-            if(k>0) k2=k;
-            sum+=9*pow(10,j);
-        }
-        int q=
- 
- 
+        
     }
- 
  
     for(int i=0; i<n; i++){
         cout << ans[i] << endl;
