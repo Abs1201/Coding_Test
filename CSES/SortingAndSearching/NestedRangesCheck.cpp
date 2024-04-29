20a2e583fa5561bf49b3bf6b4d48243a0aa255a8
KimSungHun <kimtiger1201@naver.com>
2024-04-30 01:14:49 +0900

    .

--- CSES/SortingAndSearching/NestedRangesCheck.cpp
+++ CSES/SortingAndSearching/NestedRangesCheck.cpp
@@ -1,39 +1,37 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-
-bool compare(array<ll, 5>& a1, array<ll, 5>& a2){
-    return a1[2] < a2[2];
+ 
+const int mxN=2e5;
+ 
+bool compare(array<ll, 3> a1, array<ll, 3> a2){
+    if(a1.at(0)!=a2.at(0)) return a1.at(0)<a2.at(0);
+    return a1.at(1)>a2.at(1);
 }
-
+ 
 int main(void){
     ll n;
     cin >> n;
-    vector<array<ll,5>> v;
-    for(ll i=0, a, b; i<n; i++){
-        cin >> a >> b;
-        v.push_back({a, b, i, 0, 0});
-    }
-    //sorted
-    sort(v.begin(), v.end());
-    ll b=v[0].at(1);
-    for(int i=1; i<v.size(); i++){
-        if(b>=v[i].at(1)){
-            v[i].at(4)=1;
-        }
-        else{
-            b=v[i].at(1);
-        }
+    
+    vector<array<ll,3>> v;
+    bool ans1[mxN]={0}, ans2[mxN]={0};
+    // default is 0
+    // fill_n(ans1, mxN, 0);
+    // fill_n(ans2, mxN, 0);
+ 
+    for(ll i=0; i<n; i++){
+        ll a, b;
+        cin >> a>>b;
+        v.push_back({a, b, i});
     }
     sort(v.begin(), v.end(), compare);
-    for(int i=0; i<n; i++){
-        cout << v[i].at(3) << " ";
+    
+    ll big=v[0].at(1);
+    ll small=v[0].at(1);
+    for(int i=1; i<n; i++){
+        if(v[i].at(1) <)
     }
-    cout << endl;
-    for(int i=0; i<n; i++){
-        cout << v[i].at(4) << " ";
-    }
-    cout << endl;
-
+ 
+ 
     return 0;
 }
--- CSES/SortingAndSearching/RoomAllocation.cpp
+++ CSES/SortingAndSearching/RoomAllocation.cpp
@@ -1,43 +1,43 @@
 #include <bits/stdc++.h>
-#define ll long long
+#define ll long long 
 using namespace std;
-
+ 
 const int mxN=2e5;
-
+ 
 int main(void){
     ll n;
     cin >> n;
     vector<array<ll, 3>> v;
-    for(ll i=0, a, b; i<n; i++){
+    for(ll i=0; i<n; i++){
+        ll a, b;
         cin >> a >> b;
         v.push_back({a, b, i});
     }
     sort(v.begin(), v.end());
-
-    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
-    int roomCnt=0;        
+    
     ll ans[mxN];
-    for(int i=0; i<n; i++){
-        ll t=v[i].at(1);
-        ll idx=v[i].at(2);
-        if(pq.empty() || pq.top().first >=t){
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    int roomCnt=0;
+    for(ll i=0; i<n; i++){
+        if(pq.empty() || pq.top().first >= v[i].at(0)){
             roomCnt++;
-            pq.push({t, roomCnt});
-            ans[idx]=roomCnt;
+            pq.push({v[i].at(1), roomCnt});
+            ans[v[i].at(2)]=roomCnt;
         }
         else{
-            ll bIdx=pq.top().second;
+            // cout << "pqF: " << pq.top().first << endl;
+            // cout << "vat0: " << v[i].at(0) << endl;
+            ll roomNum=pq.top().second;
             pq.pop();
-            pq.push({t, bIdx});
-            ans[idx]=bIdx;
+            pq.push({v[i].at(1), roomNum});
+            ans[v[i].at(2)]=roomNum;
         }
     }
-
+    
     cout << roomCnt << endl;
     for(int i=0; i<n; i++){
         cout << ans[i] << " ";
     }
-
-
+    
     return 0;
 }
