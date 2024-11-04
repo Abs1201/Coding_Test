c882fe98491a2ed59cef0c97b347885417d5ddcd
KimSungHun <kimtiger1201@naver.com>
2024-11-04 18:03:25 +0900

    .

--- CSES/Graph/ShortestRoutes1.cpp
+++ CSES/Graph/ShortestRoutes1.cpp
@@ -1,33 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
-
-const int mxN=1e5, mxM=2e5;
-int n,m;
+const int mxN=1e5, mxM=2e5, mxC=1e9;
+int n, m;
 vector<array<ll,2>> adj[mxN];
 ll d[mxN];
-
+bool vis[mxN];
+ 
 int main(void){
     cin >> n >> m;
-    for(int i=0; i<m; i++){
-        int a, b, c;
+    //n: node, m: line
+    for(int i=0, a, b, c; i<m; i++){
         cin >> a >> b >> c, --a, --b;
-        adj[a].push_back({c,b});
+        adj[a].push_back({c, b});
     }
-    priority_queue<array<ll, 2>, vector<array<ll,2>>, greater<array<ll,2>>> pq;
+    priority_queue<array<ll,2>, vector<array<ll,2>>, greater<array<ll,2>>> pq;
+ 
     pq.push({0,0});
+    memset(d, 0x3f, sizeof(d));
     d[0]=0;
     while(pq.size()){
-        array<ll,2> u=pq.top();
+        array<ll,2> u =pq.top();
         pq.pop();
         if(u[0]>d[u[1]]){
             continue;
         }
         for(array<ll,2> v: adj[u[1]]){
-            if(d[v[1]] > )
+            if(d[v[1]]>d[u[1]]+v[0]){
+                d[v[1]]=d[u[1]]+v[0];
+                pq.push({d[v[1]],v[1]});
+            }
         }
     }
-
+    for(int i=0; i<n; i++){
+        cout << d[i] << " ";
+    }
+ 
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define ll long long
 
-const int mxN=2e5;
+const int mxN=1e5, mxM=2e5;
+int a, b;
+int n, m, p[mxN];
 
+vector<pair<int,int>> adj;
 int main(void){
-    int n;
-    cin >> n;
-    map<int, int> m;
-    for(int i=0; i<n; i++){
-        int a;
-        cin >> a;
-        // m.insert({a, i});
-        m[a]=i;
-    }
+    // cin >> n >> m;
+    // for(int i=0; i<3; i++){
+    //     cin >> a >> b, --a, --b;
+    //     adj[a].push_back(b);
+    //     adj[b].push_back(a);
+    // }
+    adj.push_back({1,3});
 
-    int ans=0;
-    for(int i=1; i<=n; i++){
-        while(i<n && m[i] < m[i+1]){
-            i++;
-        }
-        ans++;
-    }
+    cout << adj[0].first << " " << adj[0].second << endl;
 
-    
+
 
     return 0;
 }
