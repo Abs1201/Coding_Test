9957f47813a381c958ba2f3fc1b31f4cc48635f0
KimSungHun <kimtiger1201@naver.com>
2024-11-16 13:17:41 +0900

    Update 6087.cpp

--- BJ/6087.cpp
+++ BJ/6087.cpp
@@ -20,7 +20,7 @@ int main(void){
         for(int j=0; j<w; j++){
             if(s[i][j]=='C' && !b){
                 si=i, sj=j;
-                b=!b;
+                b=1;
             }
             else if(s[i][j]=='C' && b){
                 ti=i, tj=j;
@@ -32,7 +32,6 @@ int main(void){
     memset(p, -1, sizeof(p));
     memset(ans, 0x3f, sizeof(ans));
 
-    bool vis[100][100] = {0};
     queue<pair<int,int>> qu;
     qu.push({si, sj});
     ans[si][sj]=0;
@@ -44,8 +43,8 @@ int main(void){
             int nii=ni+di[i], njj=nj+dj[i];
             if(e(nii, njj)){
                 int tmp=ans[ni][nj]+(p[ni][nj]!=i);
-                if(p[nii][njj]>tmp){
-                    p[nii][njj]=tmp;
+                if(ans[nii][njj]>tmp){
+                    ans[nii][njj]=tmp;
                     qu.push({nii, njj});
                 }
             }
